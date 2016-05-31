e8b5f5f6204871e3226cca83b3072728b2b547b3
ktan <kevin.tan@shopedia.fr>
2016-05-31 16:52:25 +0200

    Fix error in which self.opt.navigation is required to find the parent li

--- src/anchorific.js
+++ src/anchorific.js
@@ -50,7 +50,7 @@ if ( typeof Object.create !== 'function' ) {
 			if ( self.headers.length !== 0 ) {
 				self.first = parseInt( self.headers.prop( 'nodeName' ).substring( 1 ), null );
 			}
-			
+
 			self.build();
 		},
 
@@ -64,7 +64,7 @@ if ( typeof Object.create !== 'function' ) {
 			position: 'append', // position of anchor text
 			spyOffset: !0 // specify heading offset for spy scrolling
 		},
-		
+
 		build: function() {
 			var self = this, obj, navigations = function() {};
 			// when navigation configuration is set
@@ -85,10 +85,10 @@ if ( typeof Object.create !== 'function' ) {
 			if ( self.opt.spy )
 				self.spy();
 
-			if ( self.opt.top ) 
+			if ( self.opt.top )
 				self.back();
 		},
-		
+
 		navigations: function( obj ) {
 			var self = this, link, list, which, name = self.name( obj );
 
@@ -96,7 +96,7 @@ if ( typeof Object.create !== 'function' ) {
 				name = obj.attr( 'id' );
 
 			link = $( '<a />' ).attr( 'href', '#' + name ).text( obj.text() );
-			list = $( '<li />' ).append( link ); 
+			list = $( '<li />' ).append( link );
 
 			which = parseInt( obj.prop( 'nodeName' ).substring( 1 ), null );
 			list.attr( 'data-tag', which );
@@ -118,7 +118,7 @@ if ( typeof Object.create !== 'function' ) {
 				$( self.opt.navigation ).find( 'ul' ).last().append( a );
 				self.previous = a.parent();
 			} else {
-				$( 'li[data-tag=' + which + ']' ).last().parent().append( a );
+				$( self.opt.navigation ).find( 'li[data-tag=' + which + ']' ).last().parent().append( a );
 				self.previous = a.parent();
 			}
 		},
